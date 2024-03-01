58c98ad9e06fe685c9515f8da4e81424342f7b6d
artemkushch <artemkushchevskyi@gmail.com>
2024-03-01 14:06:59 +0100

    Header template

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/accordion.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="stylesheet" href="css/portfolio.css">
     <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
@@ -17,6 +21,10 @@
         </div>
       <nav class="main-nav">
         <ul class="main-menu" id="main-menu">
+        <?php
+
+
+        ?>
             <li><a href="index.html">Domov</a></li>
             <li><a href="portfolio.html">Portfólio</a></li>
             <li><a href="qna.html">Q&A</a></li>
